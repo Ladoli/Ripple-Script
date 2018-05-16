a82f1150458518ee919b6ac9e10d341fd50d116d
Ladoli <villadolidangeloryan@gmail.com>
2018-05-15 20:34:56 -0700

    Added ripple script properties

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Ripple-Script
 A script to create random ripple effects on a target container
+
+
+## Ripple-Script Configuration
+Ripple-script takes an object for its properties
+
+**duration** is an array of two numbers that determine the duration of each ripple group loop. Defaults to 21 seconds each
+
+**interval** determines how many ripples per ripple loop before restarting the loop. An interval of 10 ripples means that there will be a total of 20 ripples if each animation runs once. Defaults to 10.
+
+**randomColors** is an array of booleans that determines if ripples will have random colors. Defaults to false for both.
+
+**minRipple** determines the minimum ripple size. Defaults to 100px.
+
+**maxRipple** determines the maximum ripple size. Defaults to the smaller between width and height of the container.
+
+**rippleSpread** determines the thickness of the ripple edges. Defaults to 2px.
+
+**rippleBlur** determines the thickness of blur on the ripple edges. Defaults to 10px.
+
+**padding** determines padding wherein ripples will not start from with regard to the container edges.
+
+**defColor** determines default ripple color. Pass in strings that determine colors  css/html. Can use rgb or hexa or even strings that state the color such as 'gold'. Defaults to 'silver'.
--- rippleScript.js
+++ rippleScript.js
@@ -1,6 +1,5 @@
-function rippleAnimation(container){
+function rippleAnimation(container, properties){
   let contHeight = $(container).height();
-  console.log(contHeight)
   let contWidth = $(container).width();
   let smaller;
   if(contHeight > contWidth){
@@ -9,32 +8,75 @@ function rippleAnimation(container){
   else {
     smaller = contHeight;
   }
+  let duration = [21, 21];
+  let interval = 10;
+  let randomColors = [false,false];
+  let padding = [0,0];
+  let minRipple = 100;
+  let rippleSpread = 2;
+  let rippleBlur = 10;
+  let defColor = 'silver';
+  let maxRipple = smaller;
+
+  if(properties){
+    if(properties.duration){
+      duration = properties.duration;
+    }
+    if(properties.interval){
+      interval = properties.interval;
+    }
+    if(properties.randomColors){
+      randomColors = properties.randomColors;
+    }
+    if(properties.padding){
+      padding = properties.padding;
+    }
+    if(properties.minRipple){
+      minRipple = properties.minRipple;
+    }
+    if(properties.rippleSpread){
+      rippleSpread = properties.rippleSpread;
+    }
+    if(properties.rippleBlur){
+      rippleBlur = properties.rippleBlur;
+    }
+    if(properties.defColor){
+      defColor = properties.defColor;
+    }
+    if(properties.maxRipple){
+      maxRipple = properties.maxRipple;
+    }
+  }
+  let rippleDuration = parseInt(100/interval);
+
+
+
   let style ="";
   style +=  container + "{position: relative; text-align: center; display: flex; justify-content: center; align-items: center; overflow: hidden;} "
-            + container + '::before,'+container+"::after {content: \'\'\; position: absolute; box-shadow: 0 0 10px 2px silver; border-radius: 50%; opacity: 0;}"
-            + container +'::before {animation: ripple1 21s infinite;'
+            + container + '::before,'+container+"::after {content: \'\'\; position: absolute; box-shadow: 0 0 " + rippleBlur + "px " + rippleSpread + "px " + defColor+ "; border-radius: 50%; opacity: 0;}"
+            + container +"::before {animation: ripple1 " + duration[0] + "s infinite;"
             + 'animation-delay: 1s; }'
             + container +'::after {right: ' + randomize() + '%;'
             + 'bottom: ' + randomize() +'%;'
-            + 'animation: ripple2 21s infinite; }'
-            + generateRipple({count: 1, randomColor: true, max: smaller})
-            + generateRipple({count: 2, randomColor: false, max: smaller});
+            + "animation: ripple2 " + duration[1] + "s infinite; }"
+            + generateRipple({count: 1, randomColor: randomColors[0], max: maxRipple})
+            + generateRipple({count: 2, randomColor: randomColors[1], max: maxRipple});
 
 
-  console.log(style)
+  // console.log(style)
   $('<style>'+style+'</style>').appendTo('head');
 
   function randomize(max, min){
-    minSize = min;
+    let minSize = min;
     if(!minSize)
     {
-      minSize = 1;
+      minSize = 0;
     }
     if(!max){
-      return Math.floor((Math.random() * 100) + minSize);
+      return Math.floor((Math.random() * (100-minSize+1)) + minSize);
     }
     else{
-      return Math.floor((Math.random() * max) + minSize);
+      return Math.floor((Math.random() * (max-minSize+1)) + minSize);
     }
   }
 
@@ -45,18 +87,18 @@ function rippleAnimation(container){
     let rippleStart = "% {height: 0px; width: 0px; opacity: 1;} ";
     let resetRipple = "% {height: 0px; width: 0px; opacity: 0;} ";
     let ripple ="@keyframes ripple"+rippleStats.count + " {";
-    for(;rippleCompleted < 100;rippleCompleted+=10){
+    for(;rippleCompleted < 100;rippleCompleted+=rippleDuration){
       let color;
       if(rippleStats.randomColor){
         color = getRandomColor();
       }
       else {
-        color = "silver";
+        color = defColor;
       }
       let rippleInfo = {start:rippleCompleted, number:rippleStats.count, color:color, max: rippleStats.max};
       ripple += startRipple(rippleInfo)
                 + endRipple(rippleInfo)
-                + (rippleCompleted + 9.9) + resetRipple;
+                + (rippleCompleted + (.99*rippleDuration)) + resetRipple;
     }
     ripple += " 100% {opacity: 0;} }";
 
@@ -74,7 +116,7 @@ function rippleAnimation(container){
     function startRipple(startRippleStats){
       let vert;
       let horiz;
-      if(startRippleStats.count == 1){
+      if(startRippleStats.number == 1){
         vert = "top";
         horiz = "left";
       }
@@ -82,19 +124,21 @@ function rippleAnimation(container){
         vert = "bottom";
         horiz = "right";
       }
+      let paddingElem = parseInt(startRippleStats.number)-1;
+      let startPadding = parseInt(padding[paddingElem]);
       let rippleStart = parseInt(startRippleStats.start);
-      vertCurrent = randomize(contHeight);
-      horizCurrent = randomize(contWidth);
-      return rippleStart + "% {" + horiz + ": " + horizCurrent + "px; " + vert + ": " +  vertCurrent + "px; height: " +  0 + "px; width: " +  0 + "px; opacity: 1; box-shadow: 0 0 10px 2px " + startRippleStats.color + ";}";
+      vertCurrent = randomize(contHeight-startPadding,startPadding);
+      horizCurrent = randomize(contWidth-startPadding,startPadding);
+      return rippleStart + "% {" + horiz + ": " + horizCurrent + "px; " + vert + ": " +  vertCurrent + "px; height: " +  0 + "px; width: " +  0 + "px; opacity: 1; box-shadow: 0 0 " + rippleBlur + "px " + rippleSpread + "px " + startRippleStats.color + ";}";
 
     }
 
     function endRipple(endRippleStats) {
       let vert;
       let horiz;
-      let rippleEnd = parseInt(endRippleStats.start) + 9;
-      let rippleSize = randomize(endRippleStats.max, 50);
-      if(endRippleStats.count == 1){
+      let rippleEnd = parseInt(endRippleStats.start) + (rippleDuration*.9);
+      let rippleSize = randomize(endRippleStats.max, minRipple);
+      if(endRippleStats.number == 1){
         vert = "top";
         horiz = "left";
       }
@@ -104,8 +148,6 @@ function rippleAnimation(container){
       }
       let vertMovement = vertCurrent - rippleSize/2;
       let horizMovement = horizCurrent - rippleSize/2;
-      console.log(vertMovement)
-      console.log(horizMovement)
       return rippleEnd + "% {" + horiz + ": " + horizMovement + "px; " + vert + ": " + vertMovement + "px; height: " +  rippleSize + "px; width: " +  rippleSize + "px; opacity: 0; }";
     }
   }
